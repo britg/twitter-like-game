de8f406aacbd3cdb7e6ba85fe0802be5670d253d
britg <brit@britg.com>
2015-03-13 12:46:57 -0500

    Set up application adapter

---
+++ web/app/adapters/application.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import $ from 'jquery';
+
+ApplicationAdapter = DS.ActiveModelAdapter.extend({
+  namespace: 'api/v1',
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
+export default ApplicationAdapter;

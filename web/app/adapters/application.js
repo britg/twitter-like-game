6b7755de02bd266542b1580434859bfc1308fadd
britg <brit@britg.com>
2015-03-13 13:05:35 -0500

    Add index html back

--- web/app/adapters/application.js
+++ web/app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import $ from 'jquery';
 
-ApplicationAdapter = DS.ActiveModelAdapter.extend({
+var ApplicationAdapter = DS.ActiveModelAdapter.extend({
   namespace: 'api/v1',
   headers: {
     "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')

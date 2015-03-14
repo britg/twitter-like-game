36a9cf0c3b4012d5029bef68c48f6b8f07978086
britg <brit@britg.com>
2015-03-14 16:40:40 -0500

    remove unnecessary folders

--- web/app/components/lead-in/component.js
+++ web/app/components/lead-in/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function () {
+    console.log("lead-in component did insert element");
+  },
+  actions: {
+    startGame: function () {
+      console.log("Starting game!");
+    }
+  }
 });

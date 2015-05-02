92b19b67c9812ea7282962d8824f8c59c622a053
britg <brit@britg.com>
2015-05-02 15:04:36 -0500

    Start json build system

--- build/build.rb
+++ build/build.rb
@@ -1,3 +1,28 @@
 class Build
 
+  attr_accessor :hashes
+
+  RESOURCE_TYPES = ["locations", "npc_blueprints", "resources",
+    "combat_profiles"]
+
+  def initialize
+    @hashes = []
+  end
+
+  def load_all
+    RESOURCE_TYPES.each do |t|
+      load_json(t)
+    end
+  end
+
+  def load_json type
+    @hashes ||= {}
+    @hashes[type] ||= []
+    Dir["#{Rails.root}/build/#{type}/**/*.json"].each do |file|
+      content = open(file){ |f| f.read }
+      hash = JSON.parse(content.to_s)
+      @hashes[type] << hash
+    end
+  end
+
 end
--- lib/tasks/build.rake
+++ lib/tasks/build.rake
@@ -1,8 +1,17 @@
+require "#{Rails.root}/build/build"
+
 def y(*args) end
 namespace :build do
   desc "TODO"
+
   task clean: :environment do
     rebuild_game!
+  end
+
+  task complete: :environment do
+    b = Build.new
+    b.load_all
+    b.hashes
   end
 
 end
